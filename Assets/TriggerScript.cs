29df9672160dfb576bb3f35c05c78cb28bc3c14c
Adam Goren <amg1090@nyu.edu>
2019-09-21 17:59:46 -0400

    Wrote script for basic camera changing

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera myCamera;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Camera.main.enabled = false;
+        myCamera.enabled = true;
     }
 }
