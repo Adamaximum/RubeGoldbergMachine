bebdc2405536c44fac5d53a1eee94a8fc9ef7c65
Adam Goren <amg1090@nyu.edu>
2019-09-22 17:13:59 -0400

    Experimenting with camera code

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -27,5 +27,28 @@ public class CameraControl : MonoBehaviour
         //{
         //    Vector3 newForwardDirection = 
         //}
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if(targetOffsetX > -3)
+            {
+                targetOffsetX--;
+            }
+            if (targetOffsetY < 3)
+            {
+                targetOffsetY++;
+            }
+            if (Camera.main.transform.rotation.x < 30)
+            {
+                Camera.main.transform.Rotate(1, 0, 0);
+            }
+            if (Camera.main.transform.rotation.y < 90)
+            {
+                Camera.main.transform.Rotate(0, 1, 0);
+            }
+        }
+
+        //rotate X = 30, Y = 90
+        //offset X = -3, Y = 3
     }
 }
