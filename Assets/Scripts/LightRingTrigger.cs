b80a9d61eb02e4011295ef0818e0c8076fdcbd01
Adam Goren <amg1090@nyu.edu>
2019-09-23 00:13:39 -0400

    A little over halfway done; ready for halfway build

---
+++ Assets/Scripts/LightRingTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRingTrigger : MonoBehaviour
+{
+    public Light ring;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ring = GetComponentInChildren<Light>();
+        ring.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ring.gameObject.SetActive(true);
+    }
+}
